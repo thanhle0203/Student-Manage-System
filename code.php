95f72114f9447e6d2686734148c9f35efcacd47c
Thanh Le <thanhlevan0203@gmail.com>
2022-07-31 22:51:09 -0700

    Corrected student database

--- code.php
+++ code.php
@@ -1,15 +1,15 @@
 <?php
     require 'dbcon.php';
     if (isset($_POST['save_student'])) {
-        $name = mysqli_real_escape_string($con, $_POST['name']);
-        $email = mysqli_real_escape_string($con, $_POST['email']);
-        $phone = mysqli_real_escape_string($con, $_POST['phone']);
-        $course = mysqli_real_escape_string($con, $_POST['course']);
+        $name = mysqli_real_escape_string($conn, $_POST['name']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+        $course = mysqli_real_escape_string($conn, $_POST['course']);
 
-        $query = "INSERT INTO students (name,email,phone,course) VALUES 
+        $query = "INSERT INTO student (name,email,phone,course) VALUES 
             ('$name','$email','$phone','$course')";
         
-        $query_run = mysqli_query($con,$query);
+        $query_run = mysqli_query($conn,$query);
         if ($query_run) {
             $_SESSION['message'] = "Student Created Successfully";
             header("Location: student-create.php");
