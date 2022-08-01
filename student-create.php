65b9e43866ebbf7af7d431a5521d4ee6110c402c
Thanh Le <thanhlevan0203@gmail.com>
2022-07-31 21:47:39 -0700

    Created a file name and pasted form code to save data

--- student-create.php
+++ student-create.php
@@ -7,11 +7,15 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
+    
+    <title>Student Create</title>
+</head>
   <body>
-    <div class="container">
+    <div class="container mt-5">
+        <?php include('message.php'); ?>
+
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
