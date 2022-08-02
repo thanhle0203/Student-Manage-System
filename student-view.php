e84f301a5f6010ba3c6abb89f8c110da8b356b0c
Thanh Le <thanhlevan0203@gmail.com>
2022-08-01 23:51:57 -0700

    Included Header & Footer in includes folder

--- student-view.php
+++ student-view.php
@@ -2,18 +2,8 @@
 require 'dbcon.php';
 ?>
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php include('includes/header.php'); ?>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-
-    <title>Student View</title>
-</head>
-<body>
     <div class="container mt-5">
     <div class="row">
         <div class="col-md-12">
@@ -71,5 +61,4 @@ require 'dbcon.php';
     </div>
     </div>
     
-</body>
-</html>
+<?php include('includes/footer.php'); ?>
